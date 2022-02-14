230f8548e2cacbffb227541c1ff335ebc555586c
Timur <4kir@tim-mac.local>
2022-02-14 15:30:11 +0600

    create buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,133 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('My app'),
-        ),
-        body: Container(
-          decoration:
-              const BoxDecoration(color: Color.fromARGB(255, 194, 192, 192)),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Container(
-                child: Padding(
-                  padding: const EdgeInsets.all(30.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Text(
-                        "Email",
-                        style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(
-                          top: 5.0,
-                          bottom: 20,
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: const [
-                          Text(
-                            "Password",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "Forgot password?",
-                            style: TextStyle(
-                              color: Colors.blue,
-                              fontSize: 16,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(
-                          top: 5.0,
-                          bottom: 20,
-                        ),
-                        child: TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          color: Colors.blue,
-                        ),
-                        height: 55,
-                        width: double.infinity,
-                        child: const Center(
-                          child: Text(
-                            "Log in",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                height: 350,
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String? valueChoose;
+  List listItem = ["one", "two", "three", "four", "five"];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("My App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  overlayColor: MaterialStateProperty.all(Colors.blue)),
+              child: const Text(
+                "Text Button",
+                style: TextStyle(fontSize: 18),
               ),
-              const SizedBox(
-                height: 120,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                "Elevated Button",
+                style: TextStyle(fontSize: 18),
               ),
-              RichText(
-                text: const TextSpan(
-                  text: "Don\'t have an account? ",
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.call),
+              elevation: 10,
+              backgroundColor: Colors.green,
+              foregroundColor: Colors.yellow,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 26, right: 26),
+              child: DropdownButton(
+                hint: const Text(
+                  "Select item: ",
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
                   ),
-                  children: [
-                    TextSpan(
-                      text: "Sign up",
-                      style: TextStyle(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ],
+                ),
+                dropdownColor: Colors.lightBlueAccent,
+                iconSize: 35,
+                value: valueChoose,
+                isExpanded: true,
+                style: const TextStyle(color: Colors.black, fontSize: 18),
+                onChanged: (newValue) {
+                  setState(() {
+                    valueChoose = newValue as String?;
+                  });
+                },
+                items: listItem.map((valueItem) {
+                  return DropdownMenuItem(
+                    value: valueItem,
+                    child: Text(valueItem),
+                  );
+                }).toList(),
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              highlightColor: Colors.grey,
+              icon: const Icon(Icons.access_alarm),
+              color: const Color.fromARGB(255, 26, 13, 211),
+              iconSize: 45,
+              splashColor: Colors.amber,
+              splashRadius: 50.0,
+              tooltip: "Alarm",
+            ),
+            InkWell(
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              radius: 100,
+              splashColor: Colors.green,
+              onTap: () {},
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                width: 140,
+                height: 55,
+                child: const Center(
+                  child: Text(
+                    "Inkwell Buttom",
+                    style: TextStyle(fontSize: 18),
+                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+            OutlinedButton(
+              onPressed: () {},
+              child:
+                  const Text("Outline Button", style: TextStyle(fontSize: 18)),
+              style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.green),
+                overlayColor: MaterialStateProperty.all(Colors.amber),
+              ),
+            ),
+          ],
         ),
-        backgroundColor: Color.fromARGB(255, 161, 203, 243),
       ),
     );
   }
